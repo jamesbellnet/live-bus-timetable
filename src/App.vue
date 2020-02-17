4238ee3d07d77066103f345660e448905989d6d6
jamesbellnet <james@jamesbell.net>
2020-02-17 15:35:54 +0000

    Make API call & parse data

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,54 @@
 <template>
     <div>
         <bus-route
-            v-for="(route, index) in busRoutes"
+            v-for="(route, index) in busRoutesSliced"
             :key="index"
-            :route="route.route"
-            :destination="route.destination"
-            :expected="route.expected"
+            :route="route._links['transmodel:line'].name"
+            :destination="route.destinationName"
+            :expected="route.displayTime"
         ></bus-route>
     </div>
 </template>
 
 <script>
 import BusRoute from '@/components/BusRoute'
+import timetableApi from '@/api'
 
 export default {
     name: 'App',
+
     components: {
         'bus-route': BusRoute
     },
+
     data() {
         return {
-            busRoutes: [
-                {
-                    route: '11',
-                    destination: 'City',
-                    expected: '5mins',
-                }
-            ]
+            busRoutes: [],
+            numberToShow: 4,
+            updateBusTimes: null
         }
+    },
+
+    computed: {
+        busRoutesSliced() {
+            return this.busRoutes.slice(0, this.numberToShow)
+        }
+    },
+
+    methods: {
+        fetchBusTimes() {
+            timetableApi.get('/visits')
+                .then(res => {
+                    this.busRoutes = res.data._embedded['timetable:visit']
+                })
+                .catch(error => console.error(error))
+                .finally(() => console.info('[APP]: Bus times refreshed'))
+        }
+    },
+
+    created() {
+        this.fetchBusTimes()
+        this.updateBusTimes = setInterval(() => this.fetchBusTimes(), 60000)
     }
 }
 </script>
