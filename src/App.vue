baaf0bc244dd9dfe404a90b3ecd0a2df32d9a720
jamesbellnet <james@jamesbell.net>
2020-02-17 14:50:35 +0000

    Bus route component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
     <div>
-
+        <bus-route
+            v-for="(route, index) in busRoutes"
+            :key="index"
+            :route="route.route"
+            :destination="route.destination"
+            :expected="route.expected"
+        ></bus-route>
     </div>
 </template>
 
 <script>
+import BusRoute from '@/components/BusRoute'
+
 export default {
     name: 'App',
+    components: {
+        'bus-route': BusRoute
+    },
 }
 </script>
---
+++ src/components/BusRoute.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex-line">
+        <div class="route">{{ route }}</div>
+        <div class="destination">{{ destination }}</div>
+        <div class="expected">{{ expected }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BusRoute',
+    props: {
+        route: {
+            type: String
+        },
+        destination: {
+            type: String
+        },
+        expected: {
+            type: String
+        }
+    }
+}
+</script>
